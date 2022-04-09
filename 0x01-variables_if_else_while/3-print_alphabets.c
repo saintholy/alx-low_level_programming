04adc5d09c2e014212e8f64970955de2172414c8
saintholy <ahenkorahhasiamah@gmail.com>
2022-04-09 04:58:47 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,17 +10,17 @@ int main(void)
 char lower_alpha;
 char upper_alpha;
 
-for (upper_alpha = 'A'; upper_alpha <= 'Z'; upper_alpha++)
-{
-putchar(upper_alpha);
-}
-
 for (lower_alpha = 'a'; lower_alpha <= 'z'; lower_alpha++)
 {
 putchar(lower_alpha);
 }
 
+for (upper_alpha = 'A'; upper_alpha <= 'Z'; upper_alpha++)
+{
+putchar(upper_alpha);
+}
+
 putchar('\n');
 
-return (0)
+return (0);
 }
