b0d5c34c6a8a6690f0038d4c87b5e4af8fa0414f
saintholy <ahenkorahhasiamah@gmail.com>
2022-04-09 20:05:58 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,8 +13,8 @@ for (digit1 = 0; digit1 < 9; digit1++)
 {
 for (digit2 = digit1 = 1; digit2 < 10; digit2++)
 {
-putchar((digit1 % 30) + '0');
-putchar((digit2 % 30) + '0');
+putchar((digit1 % 50) + '0');
+putchar((digit2 % 50) + '0');
 
 if (digit1 == 8 && digit2 == 9)
 continue;
