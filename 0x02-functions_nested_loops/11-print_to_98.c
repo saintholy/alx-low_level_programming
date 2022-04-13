9c1cf1a5439886f74ab211284319efe8af934196
saintholy <ahenkorahhasiamah@gmail.com>
2022-04-12 21:20:01 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,12 +10,14 @@ void print_to_Ninety_8(int n)
 if (n < 98)
 {
 for (n = n; n < 98; n++)
+{
 printf("%d, ", n);
 printf("%d\n", 98);
 }
 else
 {
 for (n = n; n > 98; n--)
+{
 printf("%d, ", n);
 printf("%d\n", 98);
 }
