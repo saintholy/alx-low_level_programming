adc0c20a5f0be215bc818c29fd216f121d01dea9
saintholy <ahenkorahhasiamah@gmail.com>
2022-04-13 20:10:43 -0700

    Task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,8 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 
-int _putchar(char c);
-
 /**
  * print_times_table - print the times table until n
  *
@@ -11,57 +9,41 @@ int _putchar(char c);
  * Return: No return
  */
 
-void print_times_table(int n)
+void print_times_tale(int n)
 {
-	int i;
-	int j;
+int num, mult, prod;
 
-	if (n >= 0 && n <= 15)
-	{
+if (n >= 0 && n <= 15)
+{
 
-		for (i = 0; i <= n; i++)
-		{
+for (num = 0; num <= n; num++)
+{
+_putchar('0');
 
-			for (j = 0; j <= n; j++)
-			{
+for (mult = 0; mult <= n; mult++)
+{
+_putchar(',');
+_putchar(' ');
 
-				if ((i * j) < 10)
-				{
-					if (j != 0)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
-					_putchar((j * i) + '0');
-				}
+prod = num * mult;
 
-				else if ((i * j) < 100)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((((j * i) / 10) % 10) + '0');
-					_putchar(((j * i) % 10) + '0');
-				}
+if (prod <= 99)
+_putchar(' ');
+if (prod <= 9)
+_putchar(' ');
 
-				else if ((i * j) < 1000)
-				{
-					_putchar(' ');
-					_putchar((((j * i) / 100) % 10) + '0');
-					_putchar((((j * i) / 10) % 10) + '0');
-					_putchar(((j * i) % 10) + '0');
-				}
-			if (j != (n))
-			{
-				_putchar(',');
-			}
+if (prod >= 100)
+{
+_putchar((prod / 100) + '0');
+_putchar((prod /10) % 10 + '0');
 
-			}
-
-		_putchar('\n');
-
-		}
-
-	}
-
+else if (prod <= 99 && prod >= 10)
+{
+_putchar((prod / 10) + '0');
+}
+_putchar((prod % 10) + '0');
+}
+_putchar('10');
+}
+}
 }
