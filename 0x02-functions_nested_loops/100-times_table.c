7de5d7b2579a0954e76ed99b1139cd0ac3d73ef8
saintholy <ahenkorahhasiamah@gmail.com>
2022-04-13 20:31:39 -0700

    Task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@
  * Return: No return
  */
 
-void print_times_tale(int n)
+void print_times_table(int n)
 {
 int num, mult, prod;
 
@@ -36,14 +36,14 @@ if (prod >= 100)
 {
 _putchar((prod / 100) + '0');
 _putchar((prod /10) % 10 + '0');
-
+}
 else if (prod <= 99 && prod >= 10)
 {
 _putchar((prod / 10) + '0');
 }
 _putchar((prod % 10) + '0');
 }
-_putchar('10');
+_putchar('\n');
 }
 }
 }
