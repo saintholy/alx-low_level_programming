cec2965f177e606fb924e9c9ea0f2f44b6b62947
saintholy <ahenkorahhasiamah@gmail.com>
2022-04-14 20:35:09 -0700

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,38 +1,19 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-int _putchar(char c);
 
 /**
- * print_number - like a hello world
+ * print_numbers - print  0 - 9
  *
- * @n: params an integer
- *
- * No return
+ * Return: void
  */
 
-
-void print_number(int n)
+void print_numbers(void)
 {
+	char a = 0;
 
-	unsigned int n1 = 0;
-
-	if  (n < 0)
+	while (a <= 9)
 	{
-		n1 = -n;
-		_putchar('-');
+		_putchar(a + '0');
+		a++;
 	}
-
-	else
-	{
-		n1 = n;
-	}
-
-	if (n1 / 10)
-	{
-		print_number(n1 / 10);
-	}
-
-	_putchar((n1 % 10) + '0');
+	_putchar('\n');
 }
