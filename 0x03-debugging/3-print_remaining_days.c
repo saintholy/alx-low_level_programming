2e194fe072688f4886039a09628d152ae48e78d5
saintholy <ahenkorahhasiamah@gmail.com>
2022-04-14 18:18:14 -0700

    Task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 * print_remaining_days - takes a date and prints how many days are
@@ -39,3 +40,4 @@ void print_remaining_days(int month, int day, int year)
 		}
 	}
 }
+
