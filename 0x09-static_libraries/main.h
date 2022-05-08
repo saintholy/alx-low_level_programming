45062bfda939dd63b53d4dc60d23e81a0f26347c
saintholy <ahenkorahhasiamah@gmail.com>
2022-05-07 21:15:15 -0700

    Task 0

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,6 @@
-#define MAIN_H
-#define MAIN_H
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
 
 int _putchar(char c);
 int _islower(int c);
@@ -22,4 +23,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* MAIN_H *func _atoi is incomplete*/
+#endif /* FILE_MAIN *func _atoi is incomplete*/
