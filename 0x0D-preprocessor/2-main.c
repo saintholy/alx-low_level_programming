805284bb55cc652e6e61a92f225685eabff78743
saintholy <ahenkorahhasiamah@gmail.com>
2022-05-11 18:57:37 -0700

    Task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Main Header file.
+ * main - Entry point
  *
- * Return: Always 0.
- */
+ * Description: prints the name of the file it was
+ *              compiled from, followed by a new line
+ *
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
+}
