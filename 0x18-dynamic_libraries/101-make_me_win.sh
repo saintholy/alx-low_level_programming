c5255972b60cde4efd02cdf72648bb60e1174e55
saintholy <39052604+saintholy@users.noreply.github.com>
2023-01-10 15:06:46 +0000

    Task 101

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
-wget -P /tmp/ wget https://raw.githubusercontent.com/saintholy
-/
-alx-low_level_programming/master/0x18-dynamic_libraries/mao.so
-export LD_PRELOAD=/tmp/mao.so
-=======
-#!/bin/bash
-wget -P /tmp/ wget https://raw.githubusercontent.com/saintholy
-/
-alx-low_level_programming/master/0x18-dynamic_libraries/mao.so
-export LD_PRELOAD=/tmp/mao.so
->>>>>>> 4a90078bbc19f4343f900d05284dbfeddf5d48b5
+wget -P /tmp/ https://raw.github.com/ad3ldev/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
